389d4e7cd6b3e0f06e51530ab430a0ff75ad893b
AntonioSimonelli <a.simonelli@tin.it>
2023-10-07 12:02:54 +0200

    Update Algorithm.java

--- it/as/tris/Algorithm.java
+++ it/as/tris/Algorithm.java
@@ -1,1 +1,16 @@
+package it.as.tris;
+/**
+ * @(#)Algorithm.java 27.04.2014
+ * @author    Antonio Simonelli <antoniosimonelli@hotmail.com>
+ * @version   0.1,  27.04.2014
+ * Copyright 2014
+ */
+import it.as.tris.geometry.*;
+public interface Algorithm {
+	public int move();
+	public int tau(Point point);
+	public int entropy();
+	public int potential(Point point);
+	public void store(int move);
+}
 
